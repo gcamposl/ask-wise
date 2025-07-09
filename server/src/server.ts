ae821a98f558e22717c2e02ed69701bf1fa28a6d
gcamposl <gcamposlopes@gmail.com>
2025-07-09 16:33:49 -0300

    feat(server): initialize Fastify server with TypeScript and Zod type provider
    - Added package.json to manage dependencies and scripts for the server.
    - Created server.ts to set up a Fastify server with CORS support and Zod type provider.
    - Configured TypeScript with tsconfig.json for strict type checking and modern JavaScript features.

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { fastifyCors } from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5173", // or * for all origins
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app
+  .listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333 })
+  .then(() => {
+    console.log(`Port: ${process.env.PORT}`);
+    console.log("Server is running on http://localhost:3333");
+  });
